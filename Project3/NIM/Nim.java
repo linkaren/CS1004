88fd4148e59c00e170a38bdb06d206746d07dd11
linkaren <43710660+linkaren@users.noreply.github.com>
2018-09-29 20:34:36 -0400

    Create Nim.java

---
+++ Project3/NIM/Nim.java
@@ -0,0 +1,13 @@
+// Tester to see if everything works!
+
+public class Nim{
+	
+    public static void main(String[] args){
+        
+        System.out.println("Welcome to Nim death match!");
+        Game g = new Game();
+        g.play();
+        
+        System.out.println("Thanks for playing!");
+    }
+}
