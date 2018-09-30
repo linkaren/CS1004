35d57191818ff5a8f553df8717e39b803cb9adcc
linkaren <43710660+linkaren@users.noreply.github.com>
2018-09-29 20:21:22 -0400

    Create Year.java

---
+++ Project2/Year.java
@@ -0,0 +1,37 @@
+/*
+*
+* Represents a calendar year!
+*
+* Is it a leap year??
+*/
+
+public class Year {  
+    
+    // declaring instance variable
+    public int givenYear;
+    
+    // constructor
+    public Year(int y) {
+        
+        // initializing instance variable
+        givenYear = y;
+    }
+
+    public boolean isLeapYear() {
+        
+        // for years divisible by 100 but not 400
+        if (givenYear % 100 == 0 && givenYear % 400 != 0) {
+            return false; 
+        }
+        
+        // for years divisible by 4 and/or 400
+        else if (givenYear % 4 == 0 || givenYear % 400 == 0) {
+            return true;
+        }
+        
+        // for years that don't fit either the above requirements
+        else {
+            return false;
+        }
+    }
+}    
